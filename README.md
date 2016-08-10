4b652227673a2d60e72ae3ec940cf45984817896
Alexander Tsirel <noma4i@gmail.com>
2016-08-10 22:09:11 +1000

    migrations list hack

--- README.md
+++ README.md
@@ -6,21 +6,40 @@ Sublime Text plugin for better Phoenix Framework experience
 ### Quick Start
 - Clone/download plugin to your SublimeText User folder under SublimePhoenixBeagle
 
-##### Configure
-
-You can override default shortcut:
-```
-[{
-    "keys": ["command+shift+k"],
-    "command": "phoenix_beagle"
-}]
-```
-
 ### Functions
 
   - Jump from controllers to corresponding views/templates
   - View tests from controllers
   - Jump back to controller from views
+  - List Migrations
+
+##### Configure
+
+```
+[
+  { "caption": "Show More Phoenix Files", "command": "phoenix_beagle" },
+  { "caption": "Phoenix Migrations List", "command": "phoenix_beagle",
+    "args": { "command": "mlist"}
+  }
+]
+```
+
+You can override default shortcuts:
+
+```
+[
+  {
+    "keys": ["command+shift+k"],
+    "command": "phoenix_beagle"
+  },
+  {
+    "keys": ["command+shift+j"],
+    "command": "phoenix_beagle",
+    "args": { "command": "mlist"}
+  }
+]
+```
+
 
 ### Showtime!
 ![Query](showtime/phoenix_beagle.gif?raw=true)
--- phoenix_beagle.py
+++ phoenix_beagle.py
@@ -19,13 +19,24 @@ class Phoenix(object):
         files = set()
 
         file_path = self.__to_posixpath(self.__file_path)
+
         for regex, paths in list(self.__patterns.items()):
             match = re.compile(regex).match(file_path)
+
+            print ('=========')
+            print (regex)
+            print (file_path)
+            print (match)
+            print ('=========')
+
             if match:
                 files.update(self.__files_for_paths(regex, match, paths))
 
         files = list(files)
-        files.sort()
+        if 'migrations' in file_path:
+            files.sort(reverse=True)
+        else:
+            files.sort()
 
         self.__files = files
         self.__descriptions = [self.__file_without_root(file) for file in files]
@@ -37,17 +48,17 @@ class Phoenix(object):
 
     def __files_for_paths(self, regex, match, paths):
         paths = [self.__replaced_path(match, path) for path in paths]
-
         files = [glob.glob(os.path.join(self.__root, path)) for path in paths]
         flattened = [self.__to_posixpath(path) for path in list(itertools.chain.from_iterable(files))]
-
-        if self.__file_path in flattened:
-            flattened.remove(unicode(self.__file_path))
 
         return flattened
 
     def __file_without_root(self, file):
-        return os.path.basename(self.__root) + file[len(self.__root):]
+        print(self.__root)
+        clean_path = file[len(self.__root):]
+        if 'migrations' in clean_path:
+            clean_path = clean_path[len('/priv/repo/migrations/'):]
+        return clean_path
 
     def __replaced_path(self, match, path):
         replaced_path = path
@@ -59,20 +70,26 @@ class Phoenix(object):
         return re.sub("\\\\", "/", path)
 
 class PhoenixBeagleCommand(sublime_plugin.WindowCommand):
-    def run(self, index=None):
+    def run(self, index=None, **args):
+        command = args.get('command')
         active_file_path = self.__active_file_path()
         active_folders = sublime.active_window().folders()
-        if not active_folders:
-            active_folders = os.path.split(active_file_path)[0]
-            sublime.error_message("SideBar is empty!")
-        if active_file_path:
-            if self.__patterns():
-                self.__beagle = Phoenix(active_file_path, self.__patterns(), active_folders)
-                self.window.show_quick_panel(self.__beagle.descriptions(), self.__open_file)
-            else:
-                self.__status_msg("Patterns are not loaded!")
+        print(active_file_path)
+        if command == 'mlist':
+            self.__beagle = Phoenix(active_file_path + 'beagle_migrations', self.__patterns(), active_folders)
+            self.window.show_quick_panel(self.__beagle.descriptions(), self.__open_file)
         else:
-            self.__status_msg("No open files")
+            if not active_folders:
+                active_folders = os.path.split(active_file_path)[0]
+                sublime.error_message("SideBar is empty!")
+            if active_file_path:
+                if self.__patterns():
+                    self.__beagle = Phoenix(active_file_path, self.__patterns(), active_folders)
+                    self.window.show_quick_panel(self.__beagle.descriptions(), self.__open_file)
+                else:
+                    self.__status_msg("Patterns are not loaded!")
+            else:
+                self.__status_msg("No open files")
 
     def __open_file(self, index):
         if index >= 0:
